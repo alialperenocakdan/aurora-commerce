ed6455d5a11b80df4986127e87e0cc1235a6d49a
alialperenocakdan <alialperenocakdan@gmail.com>
2026-07-14 16:17:12 +0300

    There will be blood

--- product-service/src/main/java/com/aurora/product/ProductApplication.java
+++ product-service/src/main/java/com/aurora/product/ProductApplication.java
@@ -1,4 +1,4 @@
-package com.aurora.product.web;
+package com.aurora.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
